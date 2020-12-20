5dc7a1a37a1732b838df93849913fbbadef47b17
AaminaSoomro <64416973+AaminaSoomro@users.noreply.github.com>
2020-12-20 11:21:11 +0500

    reactprac

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h2>Errorsssss</h2>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          React Pagal kardega
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React And Get Headache free
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
